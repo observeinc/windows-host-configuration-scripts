7749cbaa07b19a1077d4c387615c9d5569598a11
Yasar Hussain <yasar@observeinc.com>
2022-08-11 20:55:15 -0700

    feat: refactor and pin specific version for agents

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Configuraton script for Windows
 
 ## What does it do
-Inputs: 
-Assumes existence of environment variables
---customer_id $OBSERVE_CUSTOMER_ID 
---ingest_token $OBSERVE_DATASTREAM_TOKEN 
 
-- Creates a config_files directory in home of logged in user
+- Downloads the script to the directory the command is being run from
+
+- Determines the latest version of osquery, fluentbit and telegraf
 
 - Downloads configuration files from this git repository
 
@@ -14,21 +12,21 @@ Assumes existence of environment variables
 
 - Subsitutes values for data center, hostname, customer id, data ingest token and observe endpoint in configuration files
 
-- Copies files to respective agent locations, renames existing files with suffix OLD
+- Copies files to respective agent locations, renames existing files with suffix `moved`
 
 - Outputs status of services
 
 
 ## Steps to configure
 
-1. Login to machine via ssh
+1. Login to machine via RDP
 
 1. Script flags
-    * --customer_id = your observe customer id - REQUIRED
-    * --ingest_token = your data stream ingest token from ui - REQUIRED
-    * --observe_host_name = host endpoint used in config files - OPTIONAL - defaults to collect.observeinc.com
-    * --config_files_clean = TRUE/FALSE whether to delete directory created for downloading config files - OPTIONAL - defaults to FALSE
-    * --ec2metadata = TRUE/FALSE whether to add ec2 filter section to td-agent-bit.conf file - OPTIONAL - defaults to FALSE
+    * -customer_id = your observe customer id - REQUIRED
+    * -ingest_token = your data stream ingest token from ui - REQUIRED
+    * -observe_host_name = host endpoint used in config files - OPTIONAL - defaults to collect.observeinc.com
+    * -config_files_clean = TRUE/FALSE whether to delete directory created for downloading config files - OPTIONAL - defaults to FALSE
+    * -ec2metadata = TRUE/FALSE whether to add ec2 filter section to fluent-bit.conf file - OPTIONAL - defaults to FALSE
         ```
         [FILTER]
             Name aws
@@ -41,12 +39,17 @@ Assumes existence of environment variables
             hostname true
             vpc_id true
         ```
-    * --datacenter = value to use for datacenter in td-agent-bit.conf and telegraf.conf files - OPTIONAL - defaults to AWS
-    * --appgroup = value to use for appgroup record in td-agent-bit.conf file  - OPTIONAL - defaults to null
+    * -datacenter = value to use for datacenter in fluent-bit.conf and telegraf.conf files - OPTIONAL - defaults to AWS
+    * -appgroup = value to use for appgroup record in fluent-bit.conf file  - OPTIONAL - defaults to null
+    * -local = TRUE/FLASE on whether or not use to local config files instead of the default config files - OPTIONAL - default to FALSE
+    * -force
 
 
 
 
 1. Run following script
 ```
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"; Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/agents.ps1" -outfile .\agents.ps1; .\agents.ps1 -ingest_token <<ingest_token>> -customer_id <<customer_id>>
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"; Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/agents.ps1" -outfile .\agents.ps1; .\agents.ps1  -ingest_token <<ingest_token>> -customer_id <<customer_id>> -observe_host_name <<observe_host_name>>
+
+## Using Local Configs
+If you would like to use a custom config instead of the default configs, you can use the `-local` flag and set it to `TRUE`.  If you do this, the script will look in the directory it is being run from for the files `o
--- agents.ps1
+++ agents.ps1
@@ -1,4 +1,3 @@
-######## params
 param (
     [Parameter(Mandatory)]$customer_id, 
     [Parameter(Mandatory)]$ingest_token,
@@ -6,191 +5,216 @@ param (
     $config_files_clean=$false,
     $ec2metadata=$false,
     $datacenter="aws",
-    $appgroup=$null
+    $appgroup=$null,
+    $local=$false,
+    $force=$false
     )
 
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-######## vars
-$github = "https://github.com"
-$temp_dir  = "C:\temp\observe\" #download installers here
-$msiexec = "msiexec.exe"
 
-# osquery
-$osquery_installer = "osquery.msi"
-$osquery_github_releases = "$github/osquery/osquery/releases/latest"
-$osquery_installer_path = "$temp_dir$osquery_installer"
-$osquery_destination     = "$Env:Programfiles\osquery"
-$osquery_config_template = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/osquery.conf"
-$osquery_msiexec_args = "/I ${osquery_installer_path} TARGETDIR=`"${Env:Programfiles}\osquery`" /qn"
-$osquery_flags_url = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/osquery.flags"
-
-
-# telegraf
-$telegraf_installer = "telegraf.zip"
-$telegraf_github_releases = "$github/influxdata/telegraf/releases/latest"
-$telegraf_installer_path = "$temp_dir$telegraf_installer"
-$telegraf_destination = "$Env:Programfiles\InfluxData\telegraf"
-$telegraf_config_template = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/telegraf.conf"
-
-# fluentbit
-$fluentbit_installer = "fluentbit.exe"
-$fluentbit_installer_regex = '(?!\/\w+\/)*(?!v)\d+\.\d+\.\d+(?<!\.zip)'
-$fluentbit_installer_path = "$temp_dir$fluentbit_installer"
-$fluentbit_destination = "$Env:Programfiles\fluent-bit"
-$fluentbit_config_template = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/fluent-bit.conf"
-
-# create temp dir if it doesn't exist
-Write-Host "checking for temp directory to download agents to..."
-if (-not (Test-Path -LiteralPath $temp_dir)) { 
-    try {
-        New-Item -Path $temp_dir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+    $osquery_version = "5.4.0"
+    $telegraf_version = "1.23.3"
+    $fluentbit_version = "1.9.6"
+    
+    $temp_dir  = "C:\temp\observe"
+    $osquery_msiexec_args = "/I ${temp_dir}\osquery-$osquery_version.msi TARGETDIR=```"${Env:Programfiles}\osquery```" /qn"
+    
+    $agents = @{ 
+        osquery = @{
+            AgentName = "osquery"
+            TestDestination = "${Env:Programfiles}\osquery"
+            Version = $osquery_version
+            InstallerUrl = "https://pkg.osquery.io/windows/osquery-${osquery_version}.msi"
+            DownloadDest = "$temp_dir\osquery-${osquery_version}.msi"
+            InstallationExpression = "Start-Process `"msiexec.exe`" -ArgumentList `"$osquery_msiexec_args`" -Wait -ErrorAction Stop"
+            ConfigTemplate ="https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/osquery.conf"
+            FlagsTemplate = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/osquery.flags"
+            ConfigDest = "${Env:Programfiles}\osquery\osquery.conf"
+            FlagDest = "${Env:Programfiles}\osquery\osquery.flags"
+            ServiceName = "osqueryd"
+            LocalFile = "./osquery.conf"
+            LocalFlagFile = "./osquery.flags"
+        }
+        telegraf = @{
+            AgentName = "telegraf"
+            TestDestination = "${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}"
+            Version = $telegraf_version
+            InstallerUrl = "https://dl.influxdata.com/telegraf/releases/telegraf-${telegraf_version}_windows_amd64.zip"
+            DownloadDest =  "$temp_dir\telegraf-${telegraf_version}.zip"
+            InstallationExpression = "Expand-Archive $temp_dir\telegraf-${telegraf_version}.zip -DestinationPath `"$Env:Programfiles\InfluxData\telegraf`" -Force -ErrorAction Stop"
+            ConfigTemplate = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/telegraf.conf"
+            ConfigDest = "${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}\telegraf.conf"
+            CreateServiceExpression = "Start-Process `"${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}\telegraf.exe`" -ArgumentList `"--service install --config ```"C:\Program Files\InfluxData\telegraf\telegraf-$telegraf_version\telegraf.conf```""" -Wait -ErrorAction Stop"
+            ServiceName = "telegraf"
+            LocalFile = "./telegraf.conf"
+        }
+        fluentbit = @{
+            AgentName = "fluentbit"
+            TestDestination = "${Env:Programfiles}\fluent-bit"
+            Version = $fluentbit_version
+            InstallerUrl ="https://fluentbit.io/releases/1.9/fluent-bit-${fluentbit_version}-win64.exe"
+            DownloadDest = "$temp_dir\fluent-bit-${fluentbit_version}.exe"
+            InstallationExpression = "Start-Process $temp_dir\fluent-bit-${fluentbit_version}.exe -ArgumentList `"/S /D=```"$Env:Programfiles\fluent-bit```"`" -Wait -ErrorAction Stop"
+            ConfigTemplate = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/main/fluent-bit.conf"
+            ConfigDest = "${Env:Programfiles}\fluent-bit\conf\fluent-bit.conf"
+            CreateServiceExpression = "New-Service fluent-bit -BinaryPathName `"```"${Env:Programfiles}\fluent-bit\bin\fluent-bit.exe```" -c ```"${Env:Programfiles}\fluent-bit\conf\fluent-bit.conf```"`" -StartupType Automatic -ErrorAction Stop"
+            ServiceName = "fluent-bit"
+            LocalFile = "./fluent-bit.conf"
+        }
     }
-    catch {
-        Write-Error -Message "unable to create directory '$temp_dir'. Error was: $_" -ErrorAction Stop
+
+function Create-Services {
+    
+    param(
+        $agent
+    )
+
+    if(-not (Get-Service $agent.ServiceName -ErrorAction SilentlyContinue)){
+        Write-Host "$($agent.ServiceName) does not exist, trying to create.."
+        Write-Host $agent.CreateServiceExpression
+        Invoke-Expression $agent.CreateServiceExpression
     }
-    "successfully created directory '$temp_dir'."
-}
-else {
-    "'$temp_dir' exists, moving on..."
-}
 
-######## get installer URLs
-Write-Host "determining latest osquery version..."
-$osquery_latest_uri    = (Invoke-WebRequest -UseBasicParsing $osquery_github_releases -ErrorAction Stop).Links| Where-Object {$_.href -like "*msi"} | Select-Object href
-$osquery_installer_url = $github+($osquery_latest_uri.href.ToString())
+    if(-not ((Get-Service $agent.ServiceName -ErrorAction SilentlyContinue).Status -eq "Running")){
+         Write-Host "service $($agent.ServiceName) not running, trying to restart.."
+         Stop-Service $agent.ServiceName -ErrorAction SilentlyContinue
+         Start-Sleep -Seconds 2
+         Start-service $agent.ServiceName -ErrorAction Stop
+         Get-Service $agent.ServiceName
+    }
 
-Write-Host "determining latest telegraf version..."
-$telegraf_latest_uri    = (Invoke-WebRequest -UseBasicParsing $telegraf_github_releases -ErrorAction Stop).Links | Where-Object {$_.href -like "*windows_amd64*"} | Select-Object href
-$telegraf_installer_url = $telegraf_latest_uri.href
-$telegraf_version = $telegraf_installer_url.split("/")[-1].split("_")[0]
-
-Write-Host "determining latest fluent-bit version..."
-# fluentbit doesn't always link the exe to 
-# github so we need to use the zip to get the version
-$fluentbit_latest_uri        = (Invoke-WebRequest -UseBasicParsing $github/fluent/fluent-bit/releases/latest -ErrorAction Stop).Links | Where-Object {$_.href -like "*.zip"} | Select-Object href
-$fluentbit_zip_installer_url = $github+($fluentbit_latest_uri.href.ToString())
-$fluentbit_latest_version    = [regex]::Matches($fluentbit_zip_installer_url, $fluentbit_installer_regex).Value
-$fluentbit_major_version     = $fluentbit_latest_version.split(".")
-$fluentbit_major_version     = [String]::Join('.',$fluentbit_major_version, 0,2)
-$fluentbit_installer_url     = "https://fluentbit.io/releases/${fluentbit_major_version}/fluent-bit-${fluentbit_latest_version}-win64.exe"
-$fluentbit_installer_options = "/S /D=$fluentbit_destination"
-
-######## download agents
-if (-not (Test-Path -LiteralPath $osquery_installer_path)) {
-    Write-Host "downloading osquery installer..." 
-    Invoke-WebRequest $osquery_installer_url   -OutFile $osquery_installer_path -ErrorAction Stop
-}else{
-    Write-Host "osquery installer found, skipping download..." 
-}
-if (-not (Test-Path -LiteralPath $telegraf_installer_path)) { 
-    Write-Host "downloading telegraf installer..." 
-    Invoke-WebRequest $telegraf_installer_url  -OutFile $telegraf_installer_path -ErrorAction Stop
-}else{
-    Write-Host "telegraf installer found, skipping download..." 
-}
-if (-not (Test-Path -LiteralPath $fluentbit_installer_path)) { 
-    Write-Host "downloading fluentbit installer..." 
-    Invoke-WebRequest $fluentbit_installer_url -OutFile $fluentbit_installer_path -ErrorAction Stop
-}else{
-    Write-Host "fluentbit installer found, skipping download..." 
 }
 
-######## install agents
-if (-not (Test-Path -LiteralPath $osquery_destination)) { 
-    Write-Host "installling osquery..."
-    Start-Process $msiexec -ArgumentList $osquery_msiexec_args -Wait -ErrorAction Stop
-    Write-Host "finished installing osquery.."
-}else{
-    Write-Host "osquery path $osquery_destination already exists, skipping install..."
+function Confirm-Overwrite {
+    param (
+        $agent,
+        $file
+    )
+    if($force){
+        return $true
+    }else{
+        Write-Host "found config for $agent in $file"
+        $confirmation = Read-Host "would you like to overwrite? [y/N]:"
+        if($confirmation -eq "y"){
+            Write-Host "Overwriting $file..."
+            return $true
+        }else{
+            "skipping configuration for $file"
+            return $false
+        }
+    }
 }
 
-if (-not (Test-Path -LiteralPath $telegraf_destination)) { 
-    Write-Host "installing telegraf..."
-    Expand-Archive $telegraf_installer_path -DestinationPath $telegraf_destination -Force -ErrorAction Stop
-    Write-Host "finished installing telegraf..."
-}else{
-    Write-Host "telegraf path $telegraf_destination already exists, skipping install..."
+function Configure-AgentsTemplates {
+
+    param (
+        $agent
+    )
+
+    $continue = $false
+
+    if(Test-Path -LiteralPath $agent.ConfigDest){
+        $continue = Confirm-Overwrite -agent $agent.AgentName -file $agent.ConfigDest
+    }else{
+        $continue = $true
+    }
+    if($continue){
+        $observe_host_name = $observe_host_name -Replace "https://", "" -Replace ".com/", ".com"
+        $configTemplate = if($local ){ Get-Content $agent.LocalFile} else{(Invoke-WebRequest -UseBasicParsing $agent.configTemplate).Content}
+        $configTemplate = $configTemplate -replace "<<customer_id>>", $customer_id -replace "<<ingest_token>>", $ingest_token -replace "<<observe_host_name>>" , $observe_host_name
+        if($ec2metadata){
+             $configTemplate = $configTemplate -replace "#####", ""
+             $configTemplate  = $configTemplate  -replace "  datacenter = `"aws`"", "  datacenter = `"$datacenter`""
+        }
+        if($null -ne $appgroup){
+            $configTemplate = $configTemplate -replace "#    Record appgroup ha-proxy", "    Record appgroup $appgroup"
+        }
+        if($datacenter -ne "aws"){
+            $configTemplate = $configTemplate -replace "    Record datacenter aws", "    Record datacenter $datacenter"
+            $configTemplate  = $configTemplate  -replace "  datacenter = `"aws`"", "  datacenter = `"$datacenter`""
+        }
+
+        Set-Content -Path $agent.ConfigDest -Value $configTemplate -Force -ErrorAction Stop
+    }
+    if(($agent.AgentName -eq "osquery") -and (Test-Path -LiteralPath $agent.FlagDest)){
+        $continue = Confirm-Overwrite -agent $agent.AgentName -file $agent.FlagDest
+        if($continue){
+            $osqueryFlagTemplate = if($local){ Get-Content $agent.LocalFlagFile } else{(Invoke-WebRequest -UseBasicParsing $agent.FlagsTemplate).Content}
+            Set-Content -Path $agent.FlagDest -Value $osqueryFlagTemplate -Force -ErrorAction Stop
+        }
+    }
 }
 
-if (-not (Test-Path -LiteralPath $fluentbit_destination)) { 
-    Write-Host "installing fluentbit..."
-    Start-Process $fluentbit_installer_path -ArgumentList $fluentbit_installer_options -Wait -ErrorAction Stop
-    Write-Host "finished installing fluentbit..."
-}else{
-    Write-Host "osquery path $fluentbit_destination already exists, skipping install..."
-}
-######## copy configs
-Write-Host "pulling osquery config template..."
-$osquery_conf = (Invoke-WebRequest -UseBasicParsing $osquery_config_template).Content
-$osquery_flags = (Invoke-WebRequest -UseBasicParsing $osquery_flags_url).Content
-Write-Host "pulling telegraf config template..."
-$telegraf_conf = (Invoke-WebRequest -UseBasicParsing $telegraf_config_template).Content
-Write-Host "pulling fluent-bit config template..."
-$fluentbit_conf = (Invoke-WebRequest -UseBasicParsing $fluentbit_config_template).Content
+function Download-AgentInstallers {
 
-if($ec2metadata){
-    $fluentbit_conf = $fluentbit_conf -replace "#####", ""
-    $telegraf_conf  = $telegraf_conf  -replace "  datacenter = `"aws`"", "  datacenter = `"$datacenter`""
-    $telegraf_conf = $telegraf_conf -replace "#####", ""
-}
-if($null -ne $appgroup){
-    $fluentbit_conf = $fluentbit_conf -replace "#    Record appgroup ha-proxy", "    Record appgroup $appgroup"
-}
-if($datacenter -ne "aws"){
-    $fluentbit_conf = $fluentbit_conf -replace "    Record datacenter aws", "    Record datacenter $datacenter"
-    $telegraf_conf  = $telegraf_conf  -replace "  datacenter = `"aws`"", "  datacenter = `"$datacenter`""
+    param (
+        $agent
+    )
+    if (-not (Test-Path -LiteralPath $agent.DownloadDest)) { 
+        Write-Host "downloading $($agent.AgentName) version $($agent.Version)..."
+        Invoke-WebRequest $agent.InstallerUrl -Outfile $agent.DownloadDest -ErrorAction Stop
+    }else{
+        Write-Host "found $($agent.DownloadDest), skipping download for $($agent.AgentName)..."
+    }
+
 }
 
-Write-Host "writing osquery config..."
-Set-Content -Path $osquery_destination\osquery.conf -Value $osquery_conf -Force -ErrorAction Stop
-Set-Content -Path $osquery_destination\osquery.flags -Value $osquery_flags -Force -ErrorAction Stop
+function Install-Agents {
 
-Write-Host "writing telegraf config..."
-$telegraf_conf = $telegraf_conf -replace "<<customer_id>>", $customer_id -replace "<<ingest_token>>", $ingest_token -replace "<<observe_host_name>>" , $observe_host_name
-Set-Content -Path $telegraf_destination\$telegraf_version\telegraf.conf -Value $telegraf_conf -ErrorAction Stop
+    param (
+        $agent
+    )
 
-Write-Host "writing fluent-bit config..."
-$fluentbit_conf = $fluentbit_conf -replace "<<customer_id>>", $customer_id -replace "<<ingest_token>>", $ingest_token -replace "<<observe_host_name>>" , $observe_host_name
-Set-Content -Path $fluentbit_destination\conf\fluent-bit.conf -Value $fluentbit_conf -ErrorAction Stop
+    write-host $agent.InstallationExpression
 
-# ######## spin up services
-# restart to load the new config
-if(-not (Get-Service osqueryd -ErrorAction SilentlyContinue).Status -eq "Running"){
-  Write-Host "starting osqueryd service..."
-    Start-Service osqueryd -ErrorAction Stop
-    Get-Service osqueryd
-}else{
-    Write-Host "osqueryd service found, restarting..."
-    Stop-Service osqueryd -ErrorAction Stop
-    Start-Sleep -Seconds 2
-    Start-service osqueryd -ErrorAction Stop
-    Get-Service osqueryd
+    Invoke-Expression $agent.InstallationExpression
 }
 
-if(-not (Get-Service telegraf -ErrorAction SilentlyContinue).Status -eq "Running"){
-    Write-Host "creating telegraf service..."
-    Start-Process $telegraf_destination\$telegraf_version\telegraf.exe -ArgumentList "--service install --config `"C:\Program Files\InfluxData\telegraf\$telegraf_version\telegraf.conf`"" -Wait -ErrorAction Stop
-    Write-Host "starting telegraf service..."
-    Start-Service telegraf -ErrorAction Stop
-    Get-Service telegraf
-}else{
-    Write-Host "telegraf service found, restarting..."
-    Restart-Service telegraf -ErrorAction Stop
-    Get-Service telegraf
+function Check-TempDir {
+
+    param (
+        $temp_dir
+    )
+    Write-Host "checking for temp directory to download agents to..."
+    if (-not (Test-Path -LiteralPath $temp_dir)) { 
+        try {
+            New-Item -Path $temp_dir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+        }
+        catch {
+            Write-Error -Message "unable to create directory '$temp_dir'. Error was: $_" -ErrorAction Stop
+        }
+        "successfully created directory '$temp_dir'..."
+    }
+    else {
+        "'$temp_dir' exists, moving on..."
+    }
 }
 
-if(-not (Get-Service fluent-bit -ErrorAction SilentlyContinue).Status -eq "Running"){
-    Write-Host "fluent-bit service not running, creating and starting..."
-    New-Service fluent-bit -BinaryPathName "`"$fluentbit_destination\bin\fluent-bit.exe`" -c `"$fluentbit_destination\conf\fluent-bit.conf`"" -StartupType Automatic -ErrorAction Stop
-    Start-Service fluent-bit -ErrorAction Stop
-    Get-Service fluent-bit
-} else{
-    Write-Host "fluent-bit service found, restarting..."
-    Restart-Service fluent-bit -ErrorAction Stop
-    Get-Service fluent-bit
+function Check-AgentInstalled {
+
+    param(
+        $agent
+    )
+
+    return (Test-Path $agent.TestDestination -ErrorAction SilentlyContinue)
+
+}
+
+Check-TempDir $temp_dir
+
+foreach ($agent in $agents.Keys) {
+   foreach ($agentProps in $agents[$agent]){
+        if(Check-AgentInstalled $agentProps){
+            Write-Host "$agent is already installed, skipping"
+            Continue
+        }
+        Download-AgentInstallers $agentProps -Wait
+        Install-Agents $agentProps
+        Configure-AgentsTemplates $agentProps
+        Create-Services $agentProps
+   }
 }
 
 if($config_files_clean){
     Write-Host "Removing temp files..."
     Remove-Item $temp_dir -Recurse
-}
+} 

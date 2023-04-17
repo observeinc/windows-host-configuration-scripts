1394e9d4c0b7439ca1c14f691a13ae9e97e0d76e
Jon LeBaugh <jlb@observeinc.com>
2023-04-16 19:08:22 -0500

    configdest

--- agents.ps1
+++ agents.ps1
@@ -47,10 +47,10 @@ param (
             DownloadDest =  "$temp_dir\telegraf-${telegraf_version}.zip"
             InstallationExpression = "Expand-Archive $temp_dir\telegraf-${telegraf_version}.zip -DestinationPath `"$Env:Programfiles\InfluxData\telegraf`" -Force -ErrorAction Stop"
             ConfigTemplate = "https://raw.githubusercontent.com/observeinc/windows-host-configuration-scripts/$branch/telegraf.conf"
-            ConfigDest = "${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}\"
+            ConfigDest = "${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}\conf\"
             CreateServiceExpression = "Start-Process `"${Env:Programfiles}\InfluxData\telegraf\telegraf-${telegraf_version}\telegraf.exe`" -ArgumentList `"--service install --config-directory ```"${Env:Programfiles}\InfluxData\telegraf\telegraf-$telegraf_version\conf\```""" -Wait -ErrorAction Stop"
             ServiceName = "telegraf"
-            LocalFile = "./conf/telegraf.conf"
+            LocalFile = "./telegraf.conf"
         }
         fluentbit = @{
             AgentName = "fluentbit"
